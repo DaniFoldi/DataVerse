902f8c98b41936d499061ad7d0be4a2fb68a4e25
Dániel Földi <dani@stefan.hu>
2022-12-01 18:55:24 +0000

    :recycle: Remove unused sql quad consumer

--- src/main/java/com/danifoldi/dataverse/util/SqlQuadConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.danifoldi.dataverse.util;
-
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface SqlQuadConsumer<A, B, C, D> {
-
-    void apply(A a, B b, C c, D d) throws SQLException;
-}
